689341895290c79b39f033a8d154f6ad80ff59c7
Nathan Pham <nghiapham102603@gmail.com>
2023-10-21 16:56:12 -0700

    Added User Display and Sign Out functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 
 function App() {
+  const [ user, setUser ] = useState({});
+
   function handleCallbackResponse(response) {
-    console.log(response.credential);
+    console.log("Encoded User Token: " + response.credential);
     var userObject = jwt_decode(response.credential);
     console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
   useEffect(() => {
@@ -18,11 +27,23 @@ function App() {
         document.getElementById("signInDiv"),
         { theme: "outline", size: "large" }
     );
+
+    google.accounts.id.prompt();
   }, []);
 
   return (
     <div className="App">
       <div id="signInDiv"></div>
+      { Object.keys(user).length != 0 &&
+        <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+      }
+      
+      { user &&
+        <div>
+            <img src={user.picture}></img>
+            <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }

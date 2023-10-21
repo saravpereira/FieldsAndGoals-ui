1bbfe2a18aafd5125944751a0c0323fa66661ee5
Nathan Pham <nghiapham102603@gmail.com>
2023-10-21 16:16:57 -0700

    Initial Login Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-const App = () => {
-  return <h1>Hello React!</h1>;
-};
+function App() {
+  const googleClientId = "141115154444-ffidu0bon5vvv7e1r0r0p0tqsj0oisq2.apps.googleusercontent.com"; // Replace with your Google Client ID
+
+  useEffect(() => {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: googleClientId
+      });
+    });
+  }, []);
+
+  const handleLogin = () => {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+    auth2.signIn().then(googleUser => {
+      const profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleLogin}>Login with Google</button>
+    </div>
+  );
+}
 
 export default App;

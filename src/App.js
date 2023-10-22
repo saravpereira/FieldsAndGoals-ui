6c9e84d034468663fdab9285d6876e8f7f0b2c63
Nathan Pham <nghiapham102603@gmail.com>
2023-10-21 17:14:58 -0700

    Connection to Parse Server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
+import Parse from 'parse';
 
 function App() {
   const [ user, setUser ] = useState({});
@@ -10,14 +11,34 @@ function App() {
     console.log(userObject);
     setUser(userObject);
     document.getElementById("signInDiv").hidden = true;
+
+    // Log in to Parse Server using Google token
+    Parse.User.logInWith('google', { authData: { id: userObject.sub, access_token: response.credential } })
+      .then(user => {
+        console.log('User logged in through Google:', user);
+      })
+      .catch(error => {
+        console.error('Error logging in through Google:', error);
+      });
   }
 
   function handleSignOut(event) {
-    setUser({});
-    document.getElementById("signInDiv").hidden = false;
+    // Log out from Parse Server
+    Parse.User.logOut()
+      .then(() => {
+        console.log('User logged out from Parse');
+        setUser({});
+        document.getElementById("signInDiv").hidden = false;
+      })
+      .catch(error => {
+        console.error('Error logging out:', error);
+      });
   }
 
   useEffect(() => {
+    Parse.initialize("parse"); // APP_ID
+    Parse.serverURL = 'https://api.fieldsandgoals.com/parse'; // SERVER_URL
+
     google.accounts.id.initialize({
         client_id: "141115154444-ffidu0bon5vvv7e1r0r0p0tqsj0oisq2.apps.googleusercontent.com",
         callback: handleCallbackResponse,
